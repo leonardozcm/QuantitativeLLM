9648ac500df3272d6d3545b6f03733fe996857e8
leonardozcm <leonardo1997zcm@gmail.com>
2025-02-27 01:14:01 +0800

    nms init

--- src/rms_norm/rms_norm.cu
+++ src/rms_norm/rms_norm.cu
@@ -31,8 +31,6 @@ __global__ void reset_gpu(T *m_t, const int HD = 128)
     m_t[idx] = 0.f;
 }
 
-
-
 #define VERIFY_FUNC                                                                                     \
     [&](cudaStream_t s)                                                                                 \
     {                                                                                                   \
@@ -50,62 +48,154 @@ __global__ void reset_gpu(T *m_t, const int HD = 128)
         return correct;                                                                                 \
     }
 
-
-template <typename T>
-__global__ void op(const T *input, T *output, const size_t s1, const size_t s2)
+template <int blockSize>
+__device__ void warpReduceMax(volatile float *sdata, int tid)
 {
-    int hd_idx = blockDim.x * blockIdx.x + threadIdx.x;
-    int seq_idx = blockDim.y * blockIdx.y + threadIdx.y;
-    int head_idx = blockDim.z * blockIdx.z + threadIdx.z;
-    int HD = s1;
-
-    // assert bs == 1
-    int idx = head_idx * s1 + s2 * seq_idx + hd_idx;
-
-    if (hd_idx < (HD >> 1))
-    {
-        output[idx] = input[idx] * f_cos(seq_idx, hd_idx) - input[idx + (HD >> 1)] * f_sin(seq_idx, hd_idx);
-    }
-    else
-    {
-        output[idx] = input[idx] * f_cos(seq_idx, hd_idx) + input[idx - (HD >> 1)] * f_sin(seq_idx, hd_idx);
-    }
+    if (blockSize >= 64)
+        sdata[tid] = max(sdata[tid], sdata[tid + 32]);
+    if (blockSize >= 32)
+        sdata[tid] = max(sdata[tid], sdata[tid + 16]);
+    if (blockSize >= 16)
+        sdata[tid] = max(sdata[tid], sdata[tid + 8]);
+    if (blockSize >= 8)
+        sdata[tid] = max(sdata[tid], sdata[tid + 4]);
+    ;
+    if (blockSize >= 4)
+        sdata[tid] = max(sdata[tid], sdata[tid + 2]);
+    ;
+    if (blockSize >= 2)
+        sdata[tid] = max(sdata[tid], sdata[tid + 1]);
+    ;
 }
 
+template <int blockSize>
+__device__ void warpReduceSum(volatile float *sdata, int tid)
+{
+    if (blockSize >= 64)
+        sdata[tid] += sdata[tid + 32];
+    if (blockSize >= 32)
+        sdata[tid] += sdata[tid + 16];
+    if (blockSize >= 16)
+        sdata[tid] += sdata[tid + 8];
+    if (blockSize >= 8)
+        sdata[tid] += sdata[tid + 4];
+    if (blockSize >= 4)
+        sdata[tid] += sdata[tid + 2];
+    if (blockSize >= 2)
+        sdata[tid] += sdata[tid + 1];
+}
+
+template <typename T, int BLOCK_SIZE = 256> // == blk_size
+__global__ void rms_native(const T *input, T *output, const int length)
+{
+    int seq_offset = blockIdx.x * length;
+    float *input_ptr = (float *)input + seq_offset;
+    float *output_ptr = (float *)output + seq_offset;
+
+    int tid = threadIdx.x;
+    __shared__ float maxreduce_data[BLOCK_SIZE];
+
+    // Find the maximum value in the block
+    float max_tid = 0;
+    for (int i = tid; i < length; i += blockDim.x)
+    {
+        max_tid = max_tid;
+    }
+    maxreduce_data[tid] = max_tid;
+    __syncthreads();
+
+    if (BLOCK_SIZE >= 256)
+    {
+        if (tid < 128)
+        {
+            maxreduce_data[tid] = max(maxreduce_data[tid], maxreduce_data[tid + 128]);
+        }
+        __syncthreads();
+    }
+
+    if (BLOCK_SIZE >= 128)
+    {
+        if (tid < 64)
+        {
+            maxreduce_data[tid] = max(maxreduce_data[tid], maxreduce_data[tid + 64]);
+        }
+        __syncthreads();
+    }
+
+    if (tid < 32)
+    {
+        warpReduceMax<BLOCK_SIZE>(maxreduce_data, tid);
+    }
+    __syncthreads();
+
+    float max_val = maxreduce_data[0];
+    float exp_sum = 0.f;
+
+    for (int i = tid; i < length; i += blockDim.x)
+    {
+        float val = expf(input_ptr[i] - max_val);
+        output_ptr[i] = val;
+        exp_sum += val;
+    }
+    maxreduce_data[tid] = exp_sum;
+    __syncthreads();
+
+    if (BLOCK_SIZE >= 256)
+    {
+        if (tid < 128)
+        {
+            maxreduce_data[tid] = maxreduce_data[tid] + maxreduce_data[tid + 128];
+        }
+        __syncthreads();
+    }
+
+    if (BLOCK_SIZE >= 128)
+    {
+        if (tid < 64)
+        {
+            maxreduce_data[tid] = maxreduce_data[tid] + maxreduce_data[tid + 64];
+        }
+        __syncthreads();
+    }
+
+    if (tid < 32)
+    {
+        warpReduceSum<BLOCK_SIZE>(maxreduce_data, tid);
+    }
+    __syncthreads();
+
+    exp_sum = maxreduce_data[0];
+
+    for (int i = tid; i < length; i += blockDim.x)
+    {
+        output_ptr[i] /= exp_sum; // most of them are cached in L2
+    }
+}
 
 template <typename T>
 void test_with_dtype(qttbench::State &state)
 {
-    constexpr int bs = 1, num_heads = 32, seq_len = 1024, head_dim = 128;
+    constexpr int bs = 1, seq_len = 1024, head_embed = 4096; // 32 * 128
 
-    std::vector<int> ne = {head_dim, num_heads, seq_len, bs};
+    std::vector<int> ne = {head_embed, seq_len, bs};
 
-    qttbench::Tensor<qttbench::float32_t> input(4, ne);
+    qttbench::Tensor<qttbench::float32_t> input(3, ne);
     input.initialize_random();
 
-    qttbench::Tensor<qttbench::float32_t> baseline(4, ne);
-    qttbench::Tensor<qttbench::float32_t> output(4, ne);
-
-    int32_t *pos_cpu = (int32_t *)malloc(seq_len * sizeof(int32_t));
-    for (int i = 0; i < seq_len; i++)
-    {
-        pos_cpu[i] = i;
-    }
-    qttbench::Tensor<qttbench::int32_t> pos(1, {seq_len}, pos_cpu, true);
-    free(pos_cpu);
-
+    qttbench::Tensor<qttbench::float32_t> baseline(3, ne);
+    qttbench::Tensor<qttbench::float32_t> output(3, ne);
 
     state.run(
-        "op_native",
+        "rms_norm_native",
         [&](cudaStream_t s)
         {
-            dim3 grid_size(gx, gy, gw);
-            dim3 block_size(bx, by, bw);
-            op<T><<<grid_size, block_size, 0, s>>>(
-                static_cast<const T*>(input.data_ptr()), output.data_ptr(), s1, s2);
+            constexpr int block_width = 256;
+            dim3 grid_size(seq_len);
+            dim3 block_size(block_width);
+            rms_native<T, block_width><<<grid_size, block_size, 0, s>>>(
+                static_cast<const T *>(input.data_ptr()), output.data_ptr(), context_len);
         },
         VERIFY_FUNC);
-
 }
 
 int main(int argc, char *argv[])
