88fdc4c02793f3730b4f8a614a6886196ed2760c
leonardozcm <leonardo1997zcm@gmail.com>
2025-03-12 00:11:37 +0800

    sgemm tiling partial done

--- src/sgemm/sgemm.cu
+++ src/sgemm/sgemm.cu
@@ -103,23 +103,104 @@ copy 323.416465GB/s
  * L2 cache bandwidth 1811.228600GB/s
  * smem cache bandwidth 9139.200195 GB/s
  * */
-template <int BLOCK_SIZE = 256>
+
+ // Gird_size<<<(N+N_BLOCK)/(N_BLOCK), (M+M_BLOCK)/M_BLOCK, 1>>>
+ // block_size<<<16, 16, 1>>> tx, ty
+
+ #define SIZE_OF_FLOAT4 32
+ #define FLOAT4_NUMS 4
+ #define FECTH_CONST_FLOAT4(x) *(reinterpret_cast<const float4*>(x))
+ #define FECTH_FLOAT4(x) *(reinterpret_cast<float4*>(x))
+template <int M_BLOCK=128, int N_BLOCK=128, int K_BLOCK=8, int M_THREAD=8, int N_THREAD=8>
 __global__ void sgemm_tiling_base(const float *A, const float *B, float *C, const size_t M, const size_t K, const size_t N)
 {
-    int tid = threadIdx.x;
-    int gid_x = blockIdx.x * BLOCK_SIZE;
-    int gid_y = blockIdx.y;
+    const int tx=threadIdx.x;
+    const int ty=threadIdx.y;
+    const int blk_x=blockIdx.x;
+    const int blk_y=blockIdx.y;
 
-    const float *A_ptr = A + gid_y * K;
-    const float *B_ptr = B + (gid_x + tid) * K;
-    float *C_ptr = C + gid_y * N + gid_x + tid;
+    int warp_idx = tx*ty/WARP_SIZE; // 0~8
+    // 0~2
+    int warp_lane = tx*ty%WARP_SIZE; // 0~32
+    // 0~32
+    const int warps_per_blk = blockDim.x * blockDim.y / WARP_SIZE; // 8
+    // 2
 
-    float res = 0;
-    for (int i = 0; i < K; i++)
-    {
-        res += A_ptr[i] * B_ptr[i];
+    // for warps_blk_y ~ warps_blk_x covers most slm access latency
+    const int warps_blk_y = (int)sqrtf(warps_per_blk); // 2
+    // 1: major direction
+    const int warps_blk_x = warps_per_blk / warps_blk_y; // 4
+    // 2
+
+    // For saving registers
+    // assert(M_BLOCK==N_BLOCK) and (M_THREAD==N_THERAD)
+    // so for loading A_blk and B_blk they share the same pattern
+    constexpr int warp_k_ld_in_float4_per_row = K_BLOCK / FLOAT4_NUMS; // 8/4=2
+    // 2
+    const int thread_ld_offset_in_blk_y = M_BLOCK / warps_per_blk * warp_idx + warp_lane / warp_k_ld_in_float4_per_row; // 0~128
+    // 128*(0,1)+0~16
+    const int thread_ld_offset_in_blk_x = warp_lane % warp_k_ld_in_float4_per_row;  // 0 or 1
+    // 0, 1
+    constexpr int warp_ld_cover_y_in_blk = WARP_SIZE / warp_k_ld_in_float4_per_row; // 16
+    // 16
+    const int thread_ld_times = M_BLOCK / warps_per_blk / warp_ld_cover_y_in_blk; // 1
+    // 8
+
+    constexpr int slm_padding_dim = K_BLOCK+4;
+    __shared__ float A_blk_tile[M_BLOCK][slm_padding_dim]; // avoid bank conflict, for SM Occupancy is limited by thread
+    __shared__ float B_blk_tile[N_BLOCK][slm_padding_dim]; // avoid bank conflict, for SM Occupancy is limited by thread
+
+    const float* A_blk_base_ptr = A+blk_y*M_BLOCK*K;
+    const float* B_blk_base_ptr = B+blk_x*N_BLOCK*K;
+
+    float A_thread_reg [M_THREAD];
+    float B_thread_reg [M_THREAD];
+    float C_thread_reg [M_THREAD][N_THREAD];
+    const int warp_C_offset_in_blk_row = warp_idx / warps_blk_y; // 0~4
+    // 0~2
+    const int warp_C_offset_in_blk_column = warp_idx % warps_blk_y; // 0~2
+    // 0
+    const int A_slm_warp_offset = warp_C_offset_in_blk_row * (M_BLOCK/warps_blk_x);// 0, 32, 64, 96
+    const int B_slm_warp_offset = warp_C_offset_in_blk_column * (N_BLOCK/warps_blk_y); // 0, 64
+
+    for(int k_cur=0; k_cur<K; k_cur+=K_BLOCK){
+        int t_ld_off_in_blk_y = thread_ld_offset_in_blk_y;
+
+        // load A B block to slm
+        #pragma unroll
+        for(int t_ld_time = 0; t_ld_time<thread_ld_times; t_ld_time++){
+
+            // load A blk
+            FECTH_FLOAT4(&A_blk_tile[t_ld_off_in_blk_y][thread_ld_offset_in_blk_x<<2])=
+            FECTH_CONST_FLOAT4(A_blk_base_ptr+t_ld_off_in_blk_y*K+thread_ld_offset_in_blk_x<<2);// <<2 for we already know it is a float4
+            
+            // load B blk
+            FECTH_FLOAT4(&B_blk_tile[t_ld_off_in_blk_y][thread_ld_offset_in_blk_x<<2])=
+            FECTH_CONST_FLOAT4(B_blk_base_ptr+t_ld_off_in_blk_y*K+thread_ld_offset_in_blk_x<<2);// <<2 for we already know it is a float4
+
+            t_ld_off_in_blk_y+=warp_ld_cover_y_in_blk;
+            
+        }
+        __syncthreads();
+
+        // each warp accumlate C_blk
+        #pragma unroll
+        for(int k_i=0; k_i<K_BLOCK; k_i++){
+            // load slm to A_reg
+
+            // load slm to b_reg
+
+            // perform external product on A_reg x B_reg, store to C_reg
+
+        }
+
+        A_blk_base_ptr+=K_BLOCK;
+        B_blk_base_ptr+=K_BLOCK;
     }
-    *C_ptr = res;
+
+    // write C_reg to DRAM
+
+
 }
 
 static __device__ __forceinline__ float warp_reduce_sum(float x)
@@ -140,8 +221,8 @@ void test_with_dtype(qttbench::State &state)
 
     qttbench::Tensor<qttbench::float32_t> A(3, {hidden_status_A, seq_len, bs});
     qttbench::Tensor<qttbench::float32_t> B(3, {hidden_status_A, hidden_status_B, bs});
-    // A.initialize_random();
-    // B.initialize_random();
+    A.initialize_random();
+    B.initialize_random();
 
     qttbench::Tensor<qttbench::float32_t> baseline(3, {hidden_status_B, seq_len, bs});
     qttbench::Tensor<qttbench::float32_t> C(3, {hidden_status_B, seq_len, bs});
