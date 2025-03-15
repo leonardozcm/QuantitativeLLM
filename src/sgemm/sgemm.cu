7c5ffa30903b4bbad0b003a203ac3fde89edcabf
leonardozcm <leonardo1997zcm@gmail.com>
2025-03-15 22:03:57 +0800

    solve A store bank conflict

--- src/sgemm/sgemm.cu
+++ src/sgemm/sgemm.cu
@@ -166,8 +166,8 @@ __global__ void sgemm_tiling_base(const float *A, const float *B, float *C, cons
     // 0
     const int warps_blk_A_size = M_BLOCK/warps_blk_x; // 32
     const int warps_blk_B_size = N_BLOCK/warps_blk_y; // 64
-    const int A_slm_warp_offset = ty*N_THREAD; // 0, 8, 16, 24, 32, 40, 48, ... 120(8*15)
-    const int B_slm_warp_offset = tx*M_THREAD; // 0, 8, 16, 24, 32, 40, 48, ... 120(8*15)
+    const int A_slm_warp_offset = ty*M_THREAD; // 0, 8, 16, 24, 32, 40, 48, ... 120(8*15)
+    const int B_slm_warp_offset = tx*N_THREAD; // 0, 8, 16, 24, 32, 40, 48, ... 120(8*15)
 
     // const int A_reg_ld_x_idx = tx%(warps_blk_A_size/M_THREAD)*M_THREAD;// 0, 8, 16, 24
     // const int B_reg_ld_y_idx = tx%(warps_blk_B_size/N_THREAD)*N_THREAD;// 0, 8, 16, 24...56 
@@ -237,6 +237,148 @@ __global__ void sgemm_tiling_base(const float *A, const float *B, float *C, cons
 
 }
 
+template <int M_BLOCK=128, int N_BLOCK=128, int K_BLOCK=8, int M_THREAD=8, int N_THREAD=8>
+__global__ void sgemm_tiling_optimize(const float *A, const float *B, float *C, const size_t M, const size_t K, const size_t N)
+{
+    const int tx=threadIdx.x;
+    const int ty=threadIdx.y;
+    const int blk_x=blockIdx.x;
+    const int blk_y=blockIdx.y;
+
+    int warp_idx = (tx+ty*blockDim.x)/WARP_SIZE; // 0~8
+    // 0~2
+    int warp_lane = (tx+ty*blockDim.x)%WARP_SIZE; // 0~32
+    // 0~32
+    constexpr int warps_per_blk = 8; // 8
+    // 2
+
+    // for warps_blk_y ~ warps_blk_x covers most slm access latency
+    constexpr int warps_blk_y = 2; // 2
+    // 1: major direction
+    constexpr int warps_blk_x = warps_per_blk / warps_blk_y; // 4
+    // 2
+
+    // For saving registers
+    // assert(M_BLOCK==N_BLOCK) and (M_THREAD==N_THERAD)
+    // so for loading A_blk and B_blk they share the same pattern
+    constexpr int warp_k_ld_in_float4_per_row = K_BLOCK / FLOAT4_NUMS; // 8/4=2
+    // 2
+    const int thread_ld_offset_in_blk_y = M_BLOCK / warps_per_blk * warp_idx + warp_lane / warp_k_ld_in_float4_per_row; // 0~128
+    // 128*(0,1)+0~16
+    const int thread_ld_offset_in_blk_x = warp_lane % warp_k_ld_in_float4_per_row;  // 0 or 1
+    // 0, 1
+    constexpr int warp_ld_cover_y_in_blk = WARP_SIZE / warp_k_ld_in_float4_per_row; // 16
+    // 16
+    const int thread_ld_times = M_BLOCK / warps_per_blk / warp_ld_cover_y_in_blk; // 1
+    // 8
+
+    constexpr int slm_padding_dim = K_BLOCK;
+    __shared__ float A_blk_tile[slm_padding_dim][M_BLOCK]; // avoid bank conflict, for SM Occupancy is limited by thread
+    __shared__ float B_blk_tile[N_BLOCK][slm_padding_dim]; // avoid bank conflict, for SM Occupancy is limited by thread
+
+    const float* A_blk_base_ptr = A+blk_y*M_BLOCK*K;
+    const float* B_blk_base_ptr = B+blk_x*N_BLOCK*K;
+
+    float A_thread_reg [M_THREAD] ={0.f};
+    float B_thread_reg [N_THREAD] = {0.f};
+    float C_thread_reg [M_THREAD][N_THREAD] ={0.f};
+    const int warp_C_offset_in_blk_row = warp_idx / warps_blk_y; // 0~4
+    // 0~2
+    const int warp_C_offset_in_blk_column = warp_idx % warps_blk_y; // 0~2
+    // 0
+    const int warps_blk_A_size = M_BLOCK/warps_blk_x; // 32
+    const int warps_blk_B_size = N_BLOCK/warps_blk_y; // 64
+    const int A_slm_warp_offset = ty*N_THREAD; // 0, 8, 16, 24, 32, 40, 48, ... 120(8*15)
+    const int B_slm_warp_offset = tx*M_THREAD; // 0, 8, 16, 24, 32, 40, 48, ... 120(8*15)
+
+    // const int A_reg_ld_x_idx = tx%(warps_blk_A_size/M_THREAD)*M_THREAD;// 0, 8, 16, 24
+    // const int B_reg_ld_y_idx = tx%(warps_blk_B_size/N_THREAD)*N_THREAD;// 0, 8, 16, 24...56 
+    A_blk_base_ptr += thread_ld_offset_in_blk_y*K;
+    B_blk_base_ptr += thread_ld_offset_in_blk_y*K;
+
+    const int B_x_idx = blk_x*N_BLOCK+tx*N_THREAD;
+    const int A_y_idx = blk_y*M_BLOCK+ty*M_THREAD;
+
+    const int ty_reg_offset_A = ty &1;
+
+    for(int k_cur=0; k_cur<K; k_cur+=K_BLOCK){
+
+        // load A B block to slm
+        {
+            // load A blk in reg A
+
+            // transpose and store to A slm blk
+
+            FECTH_FLOAT4(A_thread_reg) = FECTH_CONST_FLOAT4(A_blk_base_ptr+(thread_ld_offset_in_blk_x*FLOAT4_NUMS));
+            A_blk_tile[warp_idx][0*WARP_SIZE+(warp_lane>>1)+thread_ld_offset_in_blk_x*16] = reinterpret_cast<float4*>(A_thread_reg)->x;
+            A_blk_tile[warp_idx][1*WARP_SIZE+(warp_lane>>1)+thread_ld_offset_in_blk_x*16] = reinterpret_cast<float4*>(A_thread_reg)->y;
+            A_blk_tile[warp_idx][2*WARP_SIZE+(warp_lane>>1)+thread_ld_offset_in_blk_x*16] = reinterpret_cast<float4*>(A_thread_reg)->z;
+            A_blk_tile[warp_idx][3*WARP_SIZE+(warp_lane>>1)+thread_ld_offset_in_blk_x*16] = reinterpret_cast<float4*>(A_thread_reg)->w;
+
+
+            // FECTH_FLOAT4(B_thread_reg) = FECTH_CONST_FLOAT4(B_blk_base_ptr+(thread_ld_offset_in_blk_x*FLOAT4_NUMS));
+            // B_blk_tile[warp_idx][0*WARP_SIZE+thread_ld_offset_in_blk_x*16] = FECTH_FLOAT4(B_thread_reg)->x;
+            // B_blk_tile[warp_idx][1*WARP_SIZE+thread_ld_offset_in_blk_x*16] = FECTH_FLOAT4(B_thread_reg)->y;
+            // B_blk_tile[warp_idx][2*WARP_SIZE+thread_ld_offset_in_blk_x*16] = FECTH_FLOAT4(B_thread_reg)->z;
+            // B_blk_tile[warp_idx][3*WARP_SIZE+thread_ld_offset_in_blk_x*16] = FECTH_FLOAT4(B_thread_reg)->w;
+
+            // // load A blk
+            // FECTH_FLOAT4(&A_blk_tile[thread_ld_offset_in_blk_y][thread_ld_offset_in_blk_x*FLOAT4_NUMS])=
+            // FECTH_CONST_FLOAT4(A_blk_base_ptr+(thread_ld_offset_in_blk_x*FLOAT4_NUMS));// <<2 for we already know it is a float4
+            
+            // load B blk
+            FECTH_FLOAT4(&B_blk_tile[thread_ld_offset_in_blk_y][thread_ld_offset_in_blk_x*FLOAT4_NUMS])=
+            FECTH_CONST_FLOAT4(B_blk_base_ptr+(thread_ld_offset_in_blk_x*FLOAT4_NUMS));// <<2 for we already know it is a float4   
+        }
+        __syncthreads();
+
+        // each warp accumlate C_thread_reg
+        #pragma unroll
+        for(int k_i=0; k_i<K_BLOCK; k_i++){
+            // load slm to A_reg
+            int k_i_slm_blk_idx=k_i%4;
+            int k_i_slm_blk_offset=k_i/4;
+
+            // access slm in 128 bit to avoid bank conflict
+            // A_thread_reg[i] = A_blk_tile[A_slm_warp_offset+i][k_i];
+            FECTH_FLOAT4(A_thread_reg) = FECTH_CONST_FLOAT4(&A_blk_tile[warp_idx][k_i_slm_blk_idx*WARP_SIZE+k_i_slm_blk_offset*16+ty_reg_offset_A*8]);
+            FECTH_FLOAT4(A_thread_reg+FLOAT4_NUMS) = FECTH_CONST_FLOAT4(&A_blk_tile[warp_idx][k_i_slm_blk_idx*WARP_SIZE+k_i_slm_blk_offset*16+ty_reg_offset_A*8+FLOAT4_NUMS]);
+
+            
+            // load slm to b_reg
+            #pragma unroll
+            for(int i=0; i<N_THREAD; i++){
+                B_thread_reg[i] = B_blk_tile[B_slm_warp_offset+i][k_i];
+            }
+
+            // perform external product on A_reg x B_reg, store to C_reg
+            #pragma unroll
+            for(int i=0; i<M_THREAD; i++){
+                #pragma unroll
+                for(int j=0; j<N_THREAD; j++){
+                    C_thread_reg[i][j] += A_thread_reg[i]*B_thread_reg[j];
+                }
+            }
+
+        }
+        __syncthreads();
+
+        A_blk_base_ptr+=K_BLOCK;
+        B_blk_base_ptr+=K_BLOCK;
+    }
+
+    // write C_reg to DRAM
+
+    #pragma unroll
+    for(int i=0; i<M_THREAD; i++){
+        #pragma unroll
+        for(int j=0; j<N_THREAD; j++){
+            C[(A_y_idx+i)*N+B_x_idx+j] = C_thread_reg[i][j];
+        }
+    }
+
+}
+
 static __device__ __forceinline__ float warp_reduce_sum(float x)
 {
 #pragma unroll
@@ -296,22 +438,22 @@ void test_with_dtype(qttbench::State &state)
             return true;
         });
 
-    state.run(
-        "sgemm_native",
-        [&](cudaStream_t s)
-        {
-            const size_t K = hidden_status_A;
-            const size_t N = hidden_status_B;
-            const size_t M = seq_len;
-            const size_t ne2 = bs;
-            constexpr int BLOCK_SIZE = 256;
+    // state.run(
+    //     "sgemm_native",
+    //     [&](cudaStream_t s)
+    //     {
+    //         const size_t K = hidden_status_A;
+    //         const size_t N = hidden_status_B;
+    //         const size_t M = seq_len;
+    //         const size_t ne2 = bs;
+    //         constexpr int BLOCK_SIZE = 256;
 
-            dim3 grid_size((N + BLOCK_SIZE - 1) / BLOCK_SIZE, M, ne2);
-            dim3 block_size(BLOCK_SIZE, 1, 1);
-            sgemm_native<BLOCK_SIZE><<<grid_size, block_size, 0, s>>>(
-                A.data_ptr(), B.data_ptr(), C.data_ptr(), M, K, N);
-        },
-        VERIFY_FUNC);
+    //         dim3 grid_size((N + BLOCK_SIZE - 1) / BLOCK_SIZE, M, ne2);
+    //         dim3 block_size(BLOCK_SIZE, 1, 1);
+    //         sgemm_native<BLOCK_SIZE><<<grid_size, block_size, 0, s>>>(
+    //             A.data_ptr(), B.data_ptr(), C.data_ptr(), M, K, N);
+    //     },
+    //     VERIFY_FUNC);
 
     state.run(
         "sgemm_tiling 128 128 8 8 8",
@@ -329,6 +471,26 @@ void test_with_dtype(qttbench::State &state)
             dim3 block_size(BLOCK_SIZE/THREAD_SIZE, BLOCK_SIZE/THREAD_SIZE, 1);
             // cudaFuncSetAttribute(sgemm_tiling_base<BLOCK_SIZE, BLOCK_SIZE, K_BLOCK_SIZE, THREAD_SIZE, THREAD_SIZE>, cudaFuncAttributeMaxDynamicSharedMemorySize, 65536);
             sgemm_tiling_base<BLOCK_SIZE, BLOCK_SIZE, K_BLOCK_SIZE, THREAD_SIZE, THREAD_SIZE><<<grid_size, block_size, 0, s>>>(
+                A.data_ptr(), B.data_ptr(), C.data_ptr(), M, K, N);
+        },
+        VERIFY_FUNC);
+
+        state.run(
+        "sgemm_tiling optimize 128 128 8 8 8",
+        [&](cudaStream_t s)
+        {
+            const size_t K = hidden_status_A;
+            const size_t N = hidden_status_B;
+            const size_t M = seq_len;
+            const size_t ne2 = bs;
+            constexpr int BLOCK_SIZE = 128;
+            constexpr int THREAD_SIZE = 8;
+            constexpr int K_BLOCK_SIZE = 8;
+
+            dim3 grid_size((N + BLOCK_SIZE - 1) / BLOCK_SIZE, (M+BLOCK_SIZE-1)/ BLOCK_SIZE, ne2);
+            dim3 block_size(BLOCK_SIZE/THREAD_SIZE, BLOCK_SIZE/THREAD_SIZE, 1);
+            // cudaFuncSetAttribute(sgemm_tiling_base<BLOCK_SIZE, BLOCK_SIZE, K_BLOCK_SIZE, THREAD_SIZE, THREAD_SIZE>, cudaFuncAttributeMaxDynamicSharedMemorySize, 65536);
+            sgemm_tiling_optimize<BLOCK_SIZE, BLOCK_SIZE, K_BLOCK_SIZE, THREAD_SIZE, THREAD_SIZE><<<grid_size, block_size, 0, s>>>(
                 A.data_ptr(), B.data_ptr(), C.data_ptr(), M, K, N);
         },
         VERIFY_FUNC);
@@ -376,7 +538,7 @@ int main(int argc, char *argv[])
     state.set_csv_output(strutils::get_filename_without_extension(__FILE__));
 
     // test_with_dtype<qttbench::float32_t, 64, 64>(state);
-    test_with_dtype<qttbench::float32_t>(state);
+    test_with_dtype<qttbench::float32_t, 4096, 4096, 4096>(state);
     // test_with_dtype<qttbench::float32_t, 2, 4096, 14336>(state);
     // test_with_dtype<qttbench::float32_t, 4096, 14336>(state);
     return 0;
